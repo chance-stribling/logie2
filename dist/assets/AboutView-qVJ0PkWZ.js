0f47be96c5eb74a0d48036d3595f1088b4c64c55
chance-stribling <cstribling1717@gmail.com>
2023-12-16 12:09:03 -0500

    adding dist

---
+++ dist/assets/AboutView-qVJ0PkWZ.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as c}from"./index-seuSJNI1.js";const s={},n=c("h1",null,"About",-1),_=[n];function a(r,l){return o(),t("div",null,_)}const i=e(s,[["render",a]]);export{i as default};
